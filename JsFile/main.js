07d657db1dc8e23448d37e0ac232734868672ad0
Sumit-sarkar <sumitopticalit89@gmail.com>
2025-06-17 02:39:39 +0600

    proccesing 30%

--- JsFile/main.js
+++ JsFile/main.js
@@ -10,3 +10,30 @@ menuIcon.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
     navigation.style.top = "-100%"
 });
+
+
+
+// =========================video============= 
+const overlay = document.getElementById("overlay");
+const video = document.getElementById("myVideo");
+
+overlay.addEventListener("click", () => {
+      overlay.classList.add("hidden");
+      video.play();
+});
+
+
+// =======================image container========= 
+const resultImg = document.querySelectorAll('.result-img')
+
+resultImg.forEach(img => {
+    img.addEventListener("click", () => {
+        removeClass (resultImg);
+        img.classList.add('show-img')
+    })
+});
+function removeClass (resultImg){
+    resultImg.forEach(img => {
+        img.classList.remove("show-img")
+    })
+}
